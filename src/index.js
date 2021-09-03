527cf04dfeb1a2c4eee316b76743b882309e084d
michael <ittybemain@gmail.com>
2021-09-03 13:20:53 +0300

    fontawesome, :root set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import variables from "./variables.css"
+
+import brands from "../node_modules/@fortawesome/fontawesome-free/js/brands"
+import solid from "../node_modules/@fortawesome/fontawesome-free/js/solid"
+import fontawesome from "../node_modules/@fortawesome/fontawesome-free/js/fontawesome"
