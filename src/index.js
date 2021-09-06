610a9d5c3f81c4b4d78f37e6ee4a2c217241c332
michael <ittybemain@gmail.com>
2021-09-06 13:28:12 +0300

    advatanges and stay productive sections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import variables from "./variables.css"
 import cssmobile from "./mobile-styles.css"
 
-import brands from "../node_modules/@fortawesome/fontawesome-free/js/brands"
-import solid from "../node_modules/@fortawesome/fontawesome-free/js/solid"
-import fontawesome from "../node_modules/@fortawesome/fontawesome-free/js/fontawesome"
+import brands from "../node_modules/@fortawesome/fontawesome-free/js/brands.js"
+import solid from "../node_modules/@fortawesome/fontawesome-free/js/solid.js"
+import fontawesome from "../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"
