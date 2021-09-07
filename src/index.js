042a795ef3d0485e94920dfda1653d8140563d9c
michael <ittybemain@gmail.com>
2021-09-07 18:28:32 +0300

    desktop get early access

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import variables from "./variables.css"
 import cssmobile from "./mobile-styles.css"
-
+import cssdesktop from "./desktop-styles.css"
 import brands from "../node_modules/@fortawesome/fontawesome-free/js/brands.js"
 import solid from "../node_modules/@fortawesome/fontawesome-free/js/solid.js"
 import fontawesome from "../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"
