f932e68cc1761f59e8084ce311c8fb1a1fd1870b
michael <ittybemain@gmail.com>
2021-09-08 21:50:27 +0300

    js error message and check email

--- src/index.js
+++ src/index.js
@@ -4,3 +4,26 @@ import cssdesktop from "./desktop-styles.css"
 import brands from "../node_modules/@fortawesome/fontawesome-free/js/brands.js"
 import solid from "../node_modules/@fortawesome/fontawesome-free/js/solid.js"
 import fontawesome from "../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"
+
+
+function checkValidEmail () {
+    const inputText = document.getElementById("email")
+    const errorMessage = document.querySelector(".get-early-access .error-message")
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const result = re.test(inputText.value)
+    if (result){
+        errorMessage.style.display = "none"
+        console.log("result true")
+        return true
+    }
+    else {
+        errorMessage.style.display = "block"
+        console.log("result false ")
+        return false;
+    }
+}
+
+const button = document.querySelector(".get-early-access .button")
+
+button.addEventListener("click", checkValidEmail)
